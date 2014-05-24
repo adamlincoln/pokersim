0d0dd1870f56e434fa842f60fe1b8593588ccf70
Adam J. Lincoln <adamjlincoln@gmail.com>
2014-05-23 22:46:48 -0400

    A Hand class to hold the criteria for ranking hands

---
+++ src/pokersim/Hand.py
@@ -0,0 +1,104 @@
+from itertools import groupby
+from itertools import chain
+
+from Card import Card
+
+hand_rank = {
+    8: 'Straight flush',
+    7: 'Four of a kind',
+    6: 'Full house',
+    5: 'Flush',
+    4: 'Straight',
+    3: 'Three of a kind',
+    2: 'Two pair',
+    1: 'Pair',
+    0: 'High card'
+}
+
+class Hand(object):
+    def __init__(self, cards):
+        self.cards = cards
+
+    def rank(self):
+        # Can this be cached somehow?
+
+        hand = self.cards # Erm, for ease of reading?
+
+        hand.sort(cmp = Card.cmp_value)
+        # First, I need this a lot, so do it once
+        hand_values = [card.value for card in hand]
+
+        # Look for straight
+        straight = True
+        for i in xrange(len(hand_values) - 1):
+            l, r = hand_values[i:i + 2]
+            if r - l != 1:
+                straight = False
+                break
+        if hand_values == [2, 3, 4, 5, 14]: # accounts for ace low
+            straight = True
+
+        # Look for flush
+        flush = False
+        if len(set([card.suit for card in hand])) == 1:
+            flush = True
+
+        if straight:
+            value = hand_values[4]
+            if value == 14 and hand_values[0] == 2:
+                value = 5 # accounts for ace low
+            if flush:
+                # Straight flush?
+                return {'rank': 8, 'values': [value]}
+            else:
+                # Straight?
+                return {'rank': 4, 'values': [value]}
+
+        if flush:
+            # Flush?
+            return {'rank': 5, 'values': list(reversed(hand_values))}
+
+        # Look for multiples
+        multiples = {}
+        for hand_value, hand_value_iter in groupby(hand_values):
+            num = len(list(hand_value_iter))
+            if num in multiples:
+                multiples[num].append(hand_value)
+            else:
+                multiples[num] = [hand_value]
+        # Four of a kind?
+        if 4 in multiples:
+            return {'rank': 7, 'values': [multiples[4][0], multiples[1][0]]}
+        # Full house?
+        if 3 in multiples and 2 in multiples:
+            return {'rank': 6, 'values': [multiples[3][0], multiples[2][0]]}
+        # Three of a kind?
+        if 3 in multiples:
+            return {'rank': 3, 'values': chain.from_iterable([multiples[3], reversed(multiples[1])])}
+        if 2 in multiples:
+            # Two pair?
+            if len(multiples[2]) == 2:
+                return {'rank': 2, 'values': chain.from_iterable([reversed(multiples[2]), multiples[1]])}
+            # Pair?
+            if len(multiples[2]) == 1:
+                return {'rank': 1, 'values': chain.from_iterable([multiples[2], reversed(multiples[1])])}
+        # High card
+        return {'rank': 0, 'values': chain.from_iterable([reversed(multiples[1])])}
+
+    def __cmp__(self, other):
+        self.cached_rank = self.rank()
+        other.cached_rank = other.rank()
+        if self.cached_rank['rank'] < other.cached_rank['rank']:
+            return -1
+        elif self.cached_rank['rank'] > other.cached_rank['rank']:
+            return 1
+        else:
+            self_tiebreakers = list(self.cached_rank['values'])
+            other_tiebreakers = list(other.cached_rank['values'])
+            for i in xrange(len(self_tiebreakers)): # Kickers
+                if self_tiebreakers[i] < other_tiebreakers[i]:
+                    return -1
+                elif self_tiebreakers[i] > other_tiebreakers[i]:
+                    return 1
+
+        return 0
---
+++ tests/test_hand.py
@@ -0,0 +1,481 @@
+from pokersim.Hand import Hand
+from pokersim.Card import Card
+
+def test_hand_high_card_one():
+    hand1 = Hand([
+        Card('H', '2'),
+        Card('H', '5'),
+        Card('C', '6'),
+        Card('H', 'Q'),
+        Card('D', 'A')
+    ])
+    hand2 = Hand([
+        Card('H', '3'),
+        Card('H', '7'),
+        Card('C', '8'),
+        Card('C', '2'),
+        Card('D', 'K')
+    ])
+    assert hand1 > hand2
+
+def test_hand_pair_vs_high_card():
+    hand1 = Hand([
+        Card('H', '2'),
+        Card('H', '5'),
+        Card('C', 'K'),
+        Card('H', 'Q'),
+        Card('D', 'A')
+    ])
+    hand2 = Hand([
+        Card('H', '3'),
+        Card('C', '3'),
+        Card('C', '8'),
+        Card('C', '2'),
+        Card('D', 'K')
+    ])
+    assert hand1 < hand2
+
+def test_hand_pair_one():
+    hand1 = Hand([
+        Card('H', '2'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', 'A')
+    ])
+    hand2 = Hand([
+        Card('H', '3'),
+        Card('C', '3'),
+        Card('C', '8'),
+        Card('C', '2'),
+        Card('D', 'K')
+    ])
+    assert hand1 > hand2
+
+def test_hand_pair_two():
+    hand1 = Hand([
+        Card('H', '3'),
+        Card('C', '3'),
+        Card('H', '2'),
+        Card('H', '5'),
+        Card('D', 'A')
+    ])
+    hand2 = Hand([
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('C', '8'),
+        Card('C', '2'),
+        Card('D', 'K')
+    ])
+    assert hand1 < hand2
+
+def test_hand_pair_kicker():
+    hand1 = Hand([
+        Card('H', '2'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '10'),
+        Card('C', '4'),
+        Card('S', 'Q'),
+        Card('C', '2'),
+        Card('D', 'Q')
+    ])
+    assert hand1 < hand2
+
+def test_hand_two_pair_beats_one_pair():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '10'),
+        Card('C', '4'),
+        Card('S', 'A'),
+        Card('C', '2'),
+        Card('D', 'A')
+    ])
+    assert hand1 > hand2
+
+def test_hand_two_pair_top_wins():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '4'),
+        Card('C', '4'),
+        Card('S', 'A'),
+        Card('C', '2'),
+        Card('D', 'A')
+    ])
+    assert hand1 < hand2
+
+def test_hand_two_pair_bottom_wins():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '4'),
+        Card('C', '4'),
+        Card('S', 'Q'),
+        Card('C', '2'),
+        Card('D', 'Q')
+    ])
+    assert hand1 > hand2
+
+def test_hand_two_pair_kicker_wins():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', 'Q'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', '5'),
+        Card('C', '5'),
+        Card('S', 'Q'),
+        Card('C', '2'),
+        Card('D', 'Q')
+    ])
+    assert hand1 > hand2
+
+def test_hand_three_of_a_kind_beats_two_pair():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', '5'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '4'),
+        Card('C', '4'),
+        Card('S', 'A'),
+        Card('C', '2'),
+        Card('D', 'A')
+    ])
+    assert hand1 > hand2
+
+def test_hand_three_of_a_kind_value_wins():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '5'),
+        Card('C', '5'),
+        Card('H', 'Q'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('H', '4'),
+        Card('C', '7'),
+        Card('S', 'A'),
+        Card('C', 'A'),
+        Card('D', 'A')
+    ])
+    assert hand1 < hand2
+
+def test_hand_straight_beats_three_of_a_kind():
+    hand1 = Hand([
+        Card('D', '7'),
+        Card('H', '6'),
+        Card('C', '5'),
+        Card('H', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', '4'),
+        Card('C', '7'),
+        Card('S', 'A'),
+        Card('C', 'A'),
+        Card('D', 'A')
+    ])
+    assert hand1 > hand2
+
+def test_hand_ace_low_straight_beats_three_of_a_kind():
+    hand1 = Hand([
+        Card('D', '5'),
+        Card('H', '4'),
+        Card('C', '3'),
+        Card('H', '2'),
+        Card('D', 'A')
+    ])
+    hand2 = Hand([
+        Card('S', '4'),
+        Card('C', '7'),
+        Card('S', 'A'),
+        Card('C', 'A'),
+        Card('D', 'A')
+    ])
+    assert hand1 > hand2
+
+def test_hand_straight_high_card_wins():
+    hand1 = Hand([
+        Card('D', '7'),
+        Card('H', '6'),
+        Card('C', '5'),
+        Card('H', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', '7'),
+        Card('C', '8'),
+        Card('S', '9'),
+        Card('C', '10'),
+        Card('D', 'J')
+    ])
+    assert hand1 < hand2
+
+def test_hand_straight_ace_high_wins():
+    hand1 = Hand([
+        Card('D', 'A'),
+        Card('H', 'K'),
+        Card('C', 'Q'),
+        Card('H', 'J'),
+        Card('D', '10')
+    ])
+    hand2 = Hand([
+        Card('S', '7'),
+        Card('C', '8'),
+        Card('S', '9'),
+        Card('C', '10'),
+        Card('D', 'J')
+    ])
+    assert hand1 > hand2
+
+def test_hand_flush_beats_straight():
+    hand1 = Hand([
+        Card('D', 'A'),
+        Card('H', 'K'),
+        Card('C', 'Q'),
+        Card('H', 'J'),
+        Card('D', '10')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '6'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_flush_kicker_one():
+    hand1 = Hand([
+        Card('D', '10'),
+        Card('D', '7'),
+        Card('D', '5'),
+        Card('D', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '6'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_flush_kicker_two():
+    hand1 = Hand([
+        Card('D', 'J'),
+        Card('D', '7'),
+        Card('D', '5'),
+        Card('D', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '6'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_flush_kicker_three():
+    hand1 = Hand([
+        Card('D', 'J'),
+        Card('D', '8'),
+        Card('D', '5'),
+        Card('D', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '6'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_flush_kicker_four():
+    hand1 = Hand([
+        Card('D', 'J'),
+        Card('D', '8'),
+        Card('D', '5'),
+        Card('D', '3'),
+        Card('D', '2')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '5'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_flush_kicker_five():
+    hand1 = Hand([
+        Card('D', 'J'),
+        Card('D', '8'),
+        Card('D', '6'),
+        Card('D', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', 'J'),
+        Card('S', '8'),
+        Card('S', '6'),
+        Card('S', '4'),
+        Card('S', '2')
+    ])
+    assert hand1 > hand2
+
+def test_hand_full_house_beats_flush():
+    hand1 = Hand([
+        Card('D', 'J'),
+        Card('D', '8'),
+        Card('D', '6'),
+        Card('D', '4'),
+        Card('D', '3')
+    ])
+    hand2 = Hand([
+        Card('S', '10'),
+        Card('D', '10'),
+        Card('H', '10'),
+        Card('D', '2'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_full_house_high_triplet_wins():
+    hand1 = Hand([
+        Card('D', 'K'),
+        Card('C', 'K'),
+        Card('H', 'K'),
+        Card('D', '2'),
+        Card('S', '2')
+    ])
+    hand2 = Hand([
+        Card('S', '10'),
+        Card('D', '10'),
+        Card('H', '10'),
+        Card('D', '9'),
+        Card('S', '9')
+    ])
+    assert hand1 > hand2
+
+def test_hand_four_of_a_kind_beats_full_house():
+    hand1 = Hand([
+        Card('D', '4'),
+        Card('H', '4'),
+        Card('S', '4'),
+        Card('C', '4'),
+        Card('D', '9')
+    ])
+    hand2 = Hand([
+        Card('S', '10'),
+        Card('D', '10'),
+        Card('H', '10'),
+        Card('D', '2'),
+        Card('S', '2')
+    ])
+    assert hand1 > hand2
+
+def test_hand_four_of_a_kind_high_quad_wins():
+    hand1 = Hand([
+        Card('D', '4'),
+        Card('H', '4'),
+        Card('S', '4'),
+        Card('C', '4'),
+        Card('D', '9')
+    ])
+    hand2 = Hand([
+        Card('S', '10'),
+        Card('D', '10'),
+        Card('H', '10'),
+        Card('C', '10'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_straight_flush_beats_four_of_a_kind():
+    hand1 = Hand([
+        Card('D', '9'),
+        Card('H', '9'),
+        Card('S', '9'),
+        Card('C', '9'),
+        Card('D', 'K')
+    ])
+    hand2 = Hand([
+        Card('S', '6'),
+        Card('S', '5'),
+        Card('S', '4'),
+        Card('S', '3'),
+        Card('S', '2')
+    ])
+    assert hand1 < hand2
+
+def test_hand_straight_flush_highest_wins():
+    hand1 = Hand([
+        Card('H', '9'),
+        Card('H', '8'),
+        Card('H', '7'),
+        Card('H', '6'),
+        Card('H', '5')
+    ])
+    hand2 = Hand([
+        Card('S', '6'),
+        Card('S', '5'),
+        Card('S', '4'),
+        Card('S', '3'),
+        Card('S', '2')
+    ])
+    assert hand1 > hand2
+
+def test_hand_straight_tie():
+    hand1 = Hand([
+        Card('H', '9'),
+        Card('H', '8'),
+        Card('D', '7'),
+        Card('H', '6'),
+        Card('H', '5')
+    ])
+    hand2 = Hand([
+        Card('H', '9'),
+        Card('H', '8'),
+        Card('S', '7'),
+        Card('H', '6'),
+        Card('H', '5')
+    ])
+    assert not hand1 > hand2
+    assert not hand1 < hand2
+
+
