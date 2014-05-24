518ac411f4460246af9e15b956e50d14f29546ed
Adam J. Lincoln <adamjlincoln@gmail.com>
2014-05-23 22:48:21 -0400

    Card can now take int or string values and stores values as int internally.
    Card can now cmp() with itself.

--- src/pokersim/Card.py
+++ src/pokersim/Card.py
@@ -1,10 +1,27 @@
+value_map = {
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14
+}
+value_map.update(dict([reversed(i) for i in value_map.items()]))
+
 class Card(object):
     def __init__(self, suit=None, value=None):
         self.suit = suit
+        if isinstance(value, basestring):
+            if value.isdigit():
+                value = int(value)
+            else:
+                value = value_map[value]
         self.value = value
 
+    def cmp_value(self, other):
+        print 'CMPV', self, other
+        return cmp(self.value, other.value)
+        
     def __str__(self):
-        return '{0}{1}'.format(self.value, self.suit)
+        return '{0}{1}'.format(value_map[self.value] if self.value > 10 else str(self.value), self.suit)
 
     def __repr__(self):
         return '<Card {0}>'.format(self.__str__())
--- tests/test_card.py
+++ tests/test_card.py
@@ -8,7 +8,12 @@ def test_blank_card():
 def test_single_card():
     card = Card('H', '10')
     assert card.suit == 'H'
-    assert card.value == '10'
+    assert card.value == 10
+
+def test_single_card_int_value():
+    card = Card('H', 10)
+    assert card.suit == 'H'
+    assert card.value == 10
 
 def test_card_equality():
     card1 = Card('C', 'J')
@@ -28,3 +33,13 @@ def test_card_repr():
     card1 = Card('C', '10')
     assert repr(card1) == '<Card 10C>'
 
+def test_card_cmp_value():
+    card1 = Card('C', '10')
+    card2 = Card('C', 'Q')
+    assert card1.cmp_value(card2) < 0
+    assert card2.cmp_value(card1) > 0
+
+def test_card_sort_with_cmp_value():
+    cards = [Card('C', '10'), Card('S', '2'), Card('C', '5'), Card('S', 'A'), Card('H', 'K'), Card('D', 'K'), ]
+    cards.sort(cmp = Card.cmp_value)
+    assert cards == [Card('S', '2'), Card('C', '5'), Card('C', '10'), Card('H', 'K'), Card('D', 'K'), Card('S', 'A')]
