5d5d55f4b10787a6e7c210d4c3abc59fe903f4ae
Adam J. Lincoln <adamjlincoln@gmail.com>
2014-05-29 00:12:36 -0400

    Fixing up how ChipConduit handles callables are are either bound to an object or not
    Fixing tests for this too

--- src/pokersim/ChipConduit.py
+++ src/pokersim/ChipConduit.py
@@ -4,11 +4,12 @@ class ChipConduit(object):
         # publish about chip movement
         frm_attr = getattr(frm, frm_attr_name)
         to_attr = getattr(to, to_attr_name)
+        # If frm_attr is a class method, then the first arg will go along.  For tests, though, it won't, so in those I have to pass the object in via call_args.
         if hasattr(frm_attr, '__call__'):
-            frm_attr(frm, amt, *frm_call_args)
+            frm_attr(amt, *frm_call_args)
         else:
             setattr(frm, frm_attr_name, frm_attr - amt)
         if hasattr(to_attr, '__call__'):
-            to_attr(to, amt, *to_call_args)
+            to_attr(amt, *to_call_args)
         else:
             setattr(to, to_attr_name, to_attr + amt)
--- tests/test_chipconduit.py
+++ tests/test_chipconduit.py
@@ -10,20 +10,23 @@ def test_chipconduit_ints():
     assert cp1.chips == 2
     assert cp2.chips == 28
 
-def some_func(frm, amt, *other_args):
+def some_func(amt, *other_args):
+    frm = other_args[0]
     frm.chips = amt + 100
-    frm.others = other_args[0]
+    frm.others = other_args[1]
 
-def some_other_func(to, amt, *other_args):
+def some_other_func(amt, *other_args):
+    to = other_args[0]
     to.chips = amt + 2000
-    to.others = other_args[0]
+    to.others = other_args[1]
 
 def test_chipconduit_callables():
     cp1 = ChipPile('test1', 10)
     cp2 = ChipPile('test2', 20)
     cp1.some_func = some_func
     cp2.some_func2 = some_other_func # Yeah, tricky eh?
-    ChipConduit.move(8, cp1, 'some_func', cp2, 'some_func2', frm_call_args=['ASTRING'], to_call_args=[47])
+    # Those functions are unbound to an object so they have to be passed in for the purposes of this test.
+    ChipConduit.move(8, cp1, 'some_func', cp2, 'some_func2', frm_call_args=[cp1, 'ASTRING'], to_call_args=[cp2, 47])
     assert cp1.name == 'test1'
     assert cp2.name == 'test2'
     assert cp1.chips == 108
